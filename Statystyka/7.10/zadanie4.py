86de368d9df6d4b4c73480743b9f7b5c8f4c77ec
alicjaz <alza408@gmail.com>
2021-10-07 17:48:40 +0200

    7.10 - zadania ze Statystyki

--- Statystyka/7.10/zadanie4.py
+++ Statystyka/7.10/zadanie4.py
@@ -1,4 +1,18 @@
 import pandas as pd
-data = pd.read_csv('brain_size.csv', sep=";", na_values=".")
-data_pogrupowane= data.grupby("Gender")
-print(data_pogrupowane)
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import plotting
+data = pd.read_csv(r'C:\Users\Ala\PycharmProjects\dswp\Statystyka\7.10\brain_size.csv', sep=';', na_values=".")
+
+srednia = pd.DataFrame(data,columns=['VIQ']).mean()
+print("Średnia: ", srednia,"\n")
+print("Felame/Male Counter: \n", data['Gender'].value_counts())
+
+
+plotting.hist_frame(data[['VIQ','PIQ','FSIQ']])
+plt.show()
+
+woman = data[data.Gender == "Female"] 
+
+plotting.hist_frame(woman[['Weight','Height','MRI_Count']])
+plt.show()
